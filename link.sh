fabd3c7bcaf6dd7c9a9a5cfdfbbf55d1bcb3976e
Adam <adamek.og@gmail.com>
2017-07-29 20:17:41 +0200

    + added link script

---
+++ link.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+if [ -d "types" ] && [ -f "LICENSE" ] && [ -f ".gitignore" ]; then
+    current=`pwd`;
+    for dir in types/*/; do
+        if [ -d "$dir" ]; then
+            echo "$dir";
+            cd "$dir";
+            npm link;
+            cd "$current";
+        fi;
+    done;
+
+    echo "";
+    echo "------- hydrogen-core -------";
+    echo "";
+    cd "hydrogen-core";
+    npm link @types/oxygen-core;
+    npm install;
+    npm link;
+    cd "$current";
+
+    echo "";
+    echo "------- hydrogen-tile -------";
+    echo "";
+
+    cd "hydrogen-tile";
+    npm link @types/oxygen-core;
+    npm link @types/hydrogen-core;
+    npm link hydrogen-core;
+    npm install;
+    npm link;
+    cd "$current";
+
+    echo "";
+    echo "------- hydrogen-tile-test -------";
+    echo "";
+    
+    cd "hydrogen-tile-test";
+    npm link @types/oxygen-core;
+    npm link @types/hydrogen-core;
+    npm link @types/hydrogen-tile;
+    npm link hydrogen-core;
+    npm link hydrogen-tile;
+    npm install;
+    cd "$current";
+else
+    echo "Bad working directory!"
+fi;
